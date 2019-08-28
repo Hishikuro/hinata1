5ddc81048fafd4211e92fbb9908430def90eaf95
Hishikuro <54632749+Hishikuro@users.noreply.github.com>
2019-08-28 18:44:34 +0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,10 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
   console.log('I am ready!');
 });
@@ -21,6 +24,7 @@ client.on('message', message => {
     message.channel.send('pong');
   }
 });
+
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login('NjE0NDM4MjAxOTIyMTU4NTkz.XV_pVg.-V0xiKNhD9rb46OCHD0wR9sVdNk');
