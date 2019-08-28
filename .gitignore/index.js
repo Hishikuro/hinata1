3b18711a0e63120766ff1990f4e8e2fec4fb1d9d
Hishikuro <54632749+Hishikuro@users.noreply.github.com>
2019-08-28 18:42:20 +0400

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,10 +8,7 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
